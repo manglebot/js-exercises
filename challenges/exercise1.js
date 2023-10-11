ba34a2e2dfbc72ba764b30a82866a362a00475b9
Ben <manglebot@users.noreply.github.com>
2023-10-11 13:01:31 +0100

    fix:fixed error on - has M postcode

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -34,16 +34,12 @@ export function countSheep(arr) {
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
-	// M postcode and has to be Manchester!!
 
-	console.log("postCode is "+person.postCode)
-
-	if (person.city && person.postCode){
-		if(person.postCode[0] === "M" && person.city === "Manchester") {
+	if (person.address.city && person.address.postCode){
+		if(person.address.postCode[0] === "M" && person.address.city === "Manchester") {
 			return true;
 		}
 	}
-
 
 	return false;
 }
