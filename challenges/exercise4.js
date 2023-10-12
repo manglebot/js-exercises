a40689cec4104052c9683b419cdaba4215cebdce
Ben <manglebot@users.noreply.github.com>
2023-10-12 21:26:18 +0100

    feat:implemented exercise4 reverse number

--- challenges/exercise4.js
+++ challenges/exercise4.js
@@ -34,7 +34,16 @@ export const count1sand0s = (str) => {
 
 export const reverseNumber = (n) => {
 	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+	
+	return (
+		parseInt(
+			n
+			.toString()
+			.split('')
+			.reverse()
+			.join('')
+			)
+		)
 };
 
 export const sumArrays = (arrs) => {
