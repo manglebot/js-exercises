f90f613e3b670c6a33d0efdbf29622b0de7ad706
Ben <manglebot@users.noreply.github.com>
2023-10-14 14:22:58 +0100

    feat:implement exercise6 createRange function and test

--- challenges/exercise6-optional.js
+++ challenges/exercise6-optional.js
@@ -27,13 +27,17 @@ export const sumDigits = (n) => {
  * @param {Number} end
  * @param {Number} step
  */
-export const createRange = (start, end, step) => {
+export const createRange = (start, end, step = 1) => {
 	if (start === undefined) throw new Error('start is required');
 	if (end === undefined) throw new Error('end is required');
-	if (step === undefined)
-		console.log(
-			"FYI: Optional step parameter not provided. Remove this check once you've handled the optional step!"
-		);
+
+	let outputArray = [];
+
+	for (let i = start; i <= end; i+=step){
+		outputArray.push(i);
+	}
+
+	return outputArray;
 };
 
 /**
--- test/exercise6.test.js
+++ test/exercise6.test.js
@@ -1,6 +1,6 @@
 import {
 	sumDigits,
-
+    createRange,
 
 } from '../challenges/exercise6-optional';
 
@@ -15,5 +15,14 @@ describe('sumDigits', () => {
 		expect(sumDigits(123)).toBe(6);
 		expect(sumDigits(444)).toBe(12);
 		expect(sumDigits(123456789)).toBe(45);
+	});
+});
+
+describe('createRange', () => {
+	test('return array with range from start to end using steps', () => {
+		expect(createRange(3,11,2)).toEqual([ 3, 5, 7, 9, 11 ]);
+	});
+    test('return array with range from start to end using default step', () => {
+		expect(createRange(5,10)).toEqual([ 5, 6, 7, 8, 9, 10 ]);
 	});
 });
